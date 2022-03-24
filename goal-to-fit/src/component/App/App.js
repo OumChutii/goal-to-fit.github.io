824d80cf798f662f7c278b77683062e2c7378c4c
wichapol jaimaung <wichapol.44424@gmail.com>
2022-03-24 22:15:16 +0700

    Add compoment

--- goal-to-fit/src/component/App/App.js
+++ goal-to-fit/src/component/App/App.js
@@ -3,14 +3,9 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World
-        </p>
-        
-      </header>
+    <div className="">
+      <h1>Hello world</h1>
+      
     </div>
   );
 }
